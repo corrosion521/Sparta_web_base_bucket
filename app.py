fff825625d516954220a09f47620d318e41e808a
corrosion521@naver.com <corrosion521@naver.com>
2023-02-08 21:46:30 +0900

    리드미 제대로 넣기

--- app.py
+++ app.py
@@ -11,22 +11,23 @@ db = client.dbsparta
 def home():
    return render_template('index.html')
 
+# api 1 : 버킷 등록하기
 @app.route("/bucket", methods=["POST"])
 def bucket_post():
     bucket_receive = request.form['bucket_give']
 
     # 여러개 찾기 - 예시 ( _id 값은 제외하고 출력)
-    # 리스트 번호도 들어가야 하는데, 이를 위해서 이전에 쌓여 있던 리스트 가져와야함.
+    # 버킷 번호도 들어가야 하는데, 이를 위해서 이전에 쌓여 있던 버킷 리스트 가져와야함.
     bucket_list = list(db.bucket.find({}, {'_id': False}))
     count = len(bucket_list) + 1
 
     doc = {
-        'num' : count,
-        'bucket' : bucket_receive,
-        'done' : 0
+        'num' : count, # 번호
+        'bucket' : bucket_receive, #내용
+        'done' : 0 # 최초에는 완료 당연히 안되어있음.
     }
 
-    db.bucket.insert_one(doc)
+    db.bucket.insert_one(doc) # 데이터 넣기
 
     return jsonify({'msg': '등록 완료!'})
 
@@ -36,6 +37,8 @@ def bucket_done():
     print(sample_receive)
     return jsonify({'msg': 'POST(완료) 연결 완료!'})
 
+
+# api 2 : 버킷 리스트 보여주기
 @app.route("/bucket", methods=["GET"])
 def bucket_get():
     bucket_list = list(db.bucket.find({}, {'_id': False}))
